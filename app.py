6456f68111504503b18f60c9b77d6f5f1074d7f3
gitsusan <svfarago@gmail.com>
2021-04-22 19:01:49 -0600

    Fixed postgres - Kevin duct taped the Titanic!

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, redirect, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-import numpy as np
-import pandas as pd
+# import numpy as np
+# import pandas as pd
 import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.automap import automap_base
+# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import Session
 import psycopg2
@@ -14,22 +14,23 @@ import os
 #Connect using the app.config and URI to user postgres sql and db
 app = Flask(__name__)
 
+DATABASE_URL=os.environ.get('DATABASE_URL')
 # connection_string='postgres:postgres@localhost:5432/colorado_camping_db'
 # engine=create_engine(f'postgresql://{connection_string}')
-engine=create_engine(os.environ.get('DATABASE_URL').replace('://', 'ql://'))
+engine=create_engine(DATABASE_URL.replace('://', 'ql://'))
 
 # reflect an existing database into a new model
-# Base = automap_base()
-Base=declarative_base()
+Base = automap_base()
+# Base=declarative_base()
 
 # reflect the tables
-# Base.prepare(engine, reflect=True)
-
+Base.prepare(engine, reflect=True)
+# Base.metadata.create_all(engine)
 # Save references to each table
-# Reservations=Base.classes.reservations
-# Geo_Info=Base.classes.geocode_info
-# NPS_Summary=Base.classes.nps_summary
-# NPS_Comments=Base.classes.nps_comments
+Reservations=Base.classes.reservations
+Geo_Info=Base.classes.geocode_info
+NPS_Summary=Base.classes.nps_summary
+NPS_Comments=Base.classes.nps_comments
 
 #Create a home route that defines all other routes
 @app.route('/')
